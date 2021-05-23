3649c2e6cf67fb0d6e97fe72138fdfdc6adc4cbc
Ajay Suppal <akrsuppal@gmail.com>
2021-05-23 23:08:13 +0530

    initial commit

---
+++ src/main/java/com/suppal/isthesiteupservice/IsTheSiteUpServiceApplication.java
@@ -0,0 +1,13 @@
+package com.suppal.isthesiteupservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsTheSiteUpServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsTheSiteUpServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/suppal/isthesiteupservice/IsTheSiteUpServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suppal.isthesiteupservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsTheSiteUpServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
